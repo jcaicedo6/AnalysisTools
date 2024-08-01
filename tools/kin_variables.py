0e1c80bf894deff2af10750ac2a6c161157c8dd9
Johan Andres Colorado <johan.colorado@cinvestav.mx>
2024-08-02 00:44:11 +0900

    Adding Cos theta (angle between tau and hadrons in tag side)

--- tools/kin_variables.py
+++ tools/kin_variables.py
@@ -101,7 +101,7 @@ def P_smearing(P, perc):
 
 
 # Argus normalized variable calculation
-def kin_ArgusPsR(p_b,p_a, Ecms, M_mom):
+def kin_ArgusPsR(p_tracks_sig, p_tracks_tag):
     '''
     Calculates Argus normalized variable
     
@@ -112,30 +112,33 @@ def kin_ArgusPsR(p_b,p_a, Ecms, M_mom):
     :return: Argus normalized variable
     '''
     #accepted = True
-    CMS_E = Ecms
-    E_B = CMS_E/2.0
-    M_mom = M_mom
+    #CMS_E = Ecms
+    CMS_E = 10.58
+    E_mom = CMS_E/2.0
+    M_mom = 1.77709 # GeV, Belle II reported mass for the Tau Lepton 
 
     pa = TLorentzVector()
     pb = TLorentzVector()
     pa_boost = TLorentzVector()
     boostPS = TVector3()
 
-    pa = p_a
-    pb = p_b
+    pa = p_tracks_sig
+    pb = p_tracks_tag
 
-    P_B = np.sqrt(E_B*E_B - M_mom*M_mom)
-    betav = P_B/E_B
+    P_mom = np.sqrt(E_mom*E_mom - M_mom*M_mom)
+    betav = P_mom/E_mom
 
-    # Boosting of pion in the signal side to the B restframe with following approximation
-    # a) the momentum direction of the B in the 1-prong side as the opposite direction of the momentum of the D0+l in the three-prong side
+    # Boosting of pion in the signal side to the Tau restframe with following approximation
+    # a) the momentum direction of the  in the 1-prong side as the opposite direction of the momentum of the lepton in the three-prong side
     # b) the B energy by ECMS/2
     boostPS = betav*(pb.BoostVector().Unit())
     pa.Boost(boostPS)
 
     Ea_boost = pa.E()
 
-    Xval = (2.0*Ea_boost)/M_mom
+    # Argus normalized variable
+    Xval = 2.0 * Ea_boost / M_mom
+
 
     return Xval
 
@@ -162,6 +165,30 @@ def kin_q2(Ecms, px, py, pz):
     q2 = s/4 + m_sig**2 - np.sqrt(s)*Esig_CMS
 
     return q2
+
+def cos_theta_th(p_tracks_tag):
+    '''
+    Calculates cos(theta)_tau_hadron
+    
+    :param p_tracks_tag: tag side momentum
+
+    :return: cos_theta_th
+    '''
+    
+    E_mom = 10.58
+    M_mom = 1.77709
+    # Tag energy (hadrons energy in the tag side)
+    E_tag = p_tracks_tag.E()
+    # Tag momentum (hadrons momentum in the tag side)
+    p_tag = p_tracks_tag.P()
+
+    p_mom = np.sqrt(E_mom*E_mom - M_mom*M_mom)
+
+    p_nu_tag = E_mom - E_tag
+
+    cos_th = (p_mom**2 + p_tag**2 - p_nu_tag**2) / (2.0 * p_mom * p_tag) 
+
+    return cos_th
 
 # Adding Mmin2 and Mmax2 to the dataframe
 def add_Mmin2Mmax2(data):
@@ -202,3 +229,48 @@ def add_Mmin2Mmax2(data):
     data["Mmax2"] = Mmax2_list
 
     return data 
+
+def add_ArgusPsR(data):
+
+    Xval_list = []
+
+    for row in range(len(data.index)):
+
+        pa = TLorentzVector()
+        pb = TLorentzVector()
+
+        pa.SetPxPyPzE(float(data.at[row, "track_sig_px_CMS"]),
+                      float(data.at[row, "track_sig_py_CMS"]),
+                      float(data.at[row, "track_sig_pz_CMS"]),
+                      float(data.at[row, "track_sig_E_CMS"]))
+
+        pb.SetPxPyPzE(float(data.at[row, "track_tag_px_CMS"]),
+                      float(data.at[row, "track_tag_py_CMS"]),
+                      float(data.at[row, "track_tag_pz_CMS"]),
+                      float(data.at[row, "track_tag_E_CMS"]))
+
+        #Ecms = float(data.at[row, "Ecms"])
+        #Xval = kin_ArgusPsR(p_tracks_sig=pa, p_tracks_tag=pb, Ecms=Ecms)
+        Xval = kin_ArgusPsR(p_tracks_sig=pa, p_tracks_tag=pb)
+        Xval_list.append(Xval)
+        
+    data["Xps"] = Xval_list
+    return data
+
+def add_cos_theta_th(data):
+    cos_th_list = []
+    for row in range(len(data.index)):
+        
+        p_tracks_tag = TLorentzVector()
+
+        p_tracks_tag.SetPxPyPzE(float(data.at[row, "track_tag_px_CMS"]),
+                                float(data.at[row, "track_tag_py_CMS"]),
+                                float(data.at[row, "track_tag_pz_CMS"]),
+                                float(data.at[row, "track_tag_E_CMS"]))
+
+        cos_th = cos_theta_th(p_tracks_tag)
+        cos_th_list.append(cos_th)
+
+    data["cos_th"] = cos_th_list
+
+    return data
